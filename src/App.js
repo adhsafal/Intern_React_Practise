0e3fec92232c9d8a875cd9c20090a61d6f9c7c93
Safal <mr.safaladh@gmail.com>
2022-03-19 16:02:09 +0545

    ToDo and Form

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Blogs from './Blogs'
+import Form from './Form'
+import Todo from './Todo'
+
+
+const App = () => {
+  return (
+    <div>
+      {/* <Todo /> */}
+      {/* <Form /> */}
+      <Blogs />
+    </div>
+  )
+}
+
+export default App
